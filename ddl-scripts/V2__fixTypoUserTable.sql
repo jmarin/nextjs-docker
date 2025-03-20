c6035ca42e6101f9478e50675e752fd12fb5b5e4
Juan Marin Otero <juan.marin.otero@gmail.com>
2025-03-20 10:17:22 -0400

    Add basic migration

--- ddl-scripts/V2__fixTypoUserTable.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE users 
-    DROP CONSTRAINT fk_user_role,
-    RENAME COLUMN rolle TO role;
-
-ALTER TABLE users 
-    ADD CONSTRAINT fk_user_role 
-    FOREIGN KEY (role) REFERENCES roles(id);
