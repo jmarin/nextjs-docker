d4d0a55c7f82efc9bd39af2ac31bb01140cc07ef
Juan Marin Otero <juan.marin.otero@gmail.com>
2025-06-26 09:42:36 -0400

    Fix connection config

--- src/pages/api/db.cjs
+++ src/pages/api/db.cjs
@@ -1,11 +1,19 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgres://nextjs:nextjs@localhost:5432/nextjs',
+  user: process.env.PGUSER || 'nextjs',
+  host: process.env.PGHOST || 'localhost',
+  database: process.env.PGDATABASE || 'nextjs',
+  password: process.env.PGPASSWORD || 'nextjs',
+  port: process.env.PGPORT ? parseInt(process.env.PGPORT) : 5432,
 });
 
+// Always set the search_path to nextjs,public for every connection
 pool.on('connect', client => {
-  client.query('SET search_path TO nextjs');
+  client.query('SET search_path TO nextjs,public;');
 });
 
-module.exports = pool;
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  getClient: () => pool.connect(),
+};
